7392f9c3e579d2dbb29a9e1cfb074ea5a6f77649
Cesar Bouli <contato@bouli.com.br>
2021-05-05 03:50:09 -0300

    Fix bug

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,6 +1,8 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 ) 
+from werkzeug.exceptions import abort
+
 from flaskr.db import get_db
 from flaskr.auth import login_required
 
